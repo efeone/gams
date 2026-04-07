f7074a7914afcba6dcbfc4fbdc3da1efee78932e
NaseeraVP <naseeravp38@gmail.com>
2026-04-07 14:00:20 +0530

    feat:created GAMS Manager role

--- gams/general_administration_management_system/doctype/incident_report/incident_report.js
+++ gams/general_administration_management_system/doctype/incident_report/incident_report.js
@@ -2,16 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Incident Report', {
-    onload: function(frm) {
-        if (!frm.doc.reported_by) {
-            frappe.db.get_value('Employee', 
-                { user_id: frappe.session.user }, 
-                'name'
-            ).then(r => {
-                if (r.message) {
-                    frm.set_value('reported_by', r.message.name);
-                }
-            });
-        }
-    }
+	onload: function(frm) {
+		if (!frm.doc.reported_by) {
+			frappe.db.get_value('Employee', 
+				{ user_id: frappe.session.user }, 
+				'name'
+			).then(r => {
+				if (r.message) {
+					frm.set_value('reported_by', r.message.name);
+				}
+			});
+		}
+	}
 });
