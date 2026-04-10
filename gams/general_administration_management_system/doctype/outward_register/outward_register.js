5452d4eda8134ee1a7b9e89e972f82cd1d9b76d4
NaseeraVP <naseeravp38@gmail.com>
2026-04-10 10:45:20 +0530

    feat: Added Inward Register and Outward Register doctype

---
+++ gams/general_administration_management_system/doctype/outward_register/outward_register.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, efeone and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Outward Register", {
+// 	refresh(frm) {
+
+// 	},
+// });
